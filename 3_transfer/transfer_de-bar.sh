3e69b0ac369f855b5dbcd03274ca34f98caa14be
whher <82605078+whher@users.noreply.github.com>
2023-03-08 16:11:16 +0100

    Add files via upload

---
+++ 3_transfer/transfer_de-bar.sh
@@ -0,0 +1,52 @@
+#! usr/bin/bash
+
+# System: Transfer
+# Direction: de-bar
+
+export PYTHONPATH=$(pwd)/subword-nmt:$PYTHONPATH
+pip install torch==1.11.0
+pip install sacrebleu==1.4.14
+
+# wrapper for cross validation
+
+train_it=("1" "2" "3" "4" "5")
+train_it2=("4" "5" "1" "2" "3")
+test_it=("5" "1" "2" "3" "4")
+
+for ((i=0;i<=4;i++))
+do
+    #echo ${train_it[i]}
+    #echo ${train_it2[i]}
+    #echo ${test_it[i]}
+    
+    cv_fold="de-bar${train_it[i]}-${train_it2[i]}"
+    source_train="de${train_it[i]}-${train_it2[i]}"
+    target_train="bar${train_it[i]}-${train_it2[i]}"
+    source_test="de${test_it[i]}"
+    target_test="bar${test_it[i]}"
+
+    echo $cv_fold
+
+# joint bpe already learned
+
+# apply bpe
+
+    python -m apply_bpe -c model_transfer_cv/bpe.codes --dropout 0.1 --vocabulary model_transfer_cv/bpe.vocab.de --vocabulary-threshold 50 < corpus/parallel/bar-de/$source_train > model_transfer_cv/$cv_fold/child.train.BPE.de
+
+    python -m apply_bpe -c model_transfer_cv/bpe.codes --dropout 0.1 --vocabulary model_transfer_cv/bpe.vocab.bar --vocabulary-threshold 50 < corpus/parallel/bar-de/$target_train > model_transfer_cv/$cv_fold/child.train.BPE.bar
+
+    python -m apply_bpe -c model_transfer_cv/bpe.codes --vocabulary model_transfer_cv/bpe.vocab.de --vocabulary-threshold 50 < corpus/parallel/bar-de/$source_test > model_transfer_cv/$cv_fold/child.test.BPE.de
+
+    python -m apply_bpe -c model_transfer_cv/bpe.codes --vocabulary model_transfer_cv/bpe.vocab.bar --vocabulary-threshold 50 < corpus/parallel/bar-de/$target_test > model_transfer_cv/$cv_fold/child.test.BPE.bar
+
+# prepare data for child
+
+    python -m sockeye.prepare_data -s model_transfer_cv/$cv_fold/child.train.BPE.de -t model_transfer_cv/$cv_fold/child.train.BPE.bar -o model_transfer_cv/$cv_fold/train_data_child_$cv_fold --source-vocab model_transfer_cv/train_data_de-fr_parent/vocab.src.0.json --target-vocab model_transfer_cv/train_data_de-fr_parent/vocab.trg.0.json --shared-vocab
+
+# continue training
+
+    python -m sockeye.train --config model_transfer_cv/de-fr-parent-model/args.yaml -d model_transfer_cv/$cv_fold/train_data_child_$cv_fold -vs model_transfer_cv/$cv_fold/child.test.BPE.de -vt model_transfer_cv/$cv_fold/child.test.BPE.bar --params model_transfer_cv/de-fr-parent-model/params.best -o model_transfer_cv/$cv_fold/de-bar-child-model
+
+done
+
+
